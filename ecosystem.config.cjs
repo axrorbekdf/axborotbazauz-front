6269471c7240e0f9eae7f1c937dd3baa8e1cc01f
Ahrorbek Davronov <axrorbektatu@gmail.com>
2025-02-14 10:01:28 +0500

    new updates

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,14 +1,13 @@
-
 module.exports = {
-    apps: [
-      {
-        name: "nuxt-app",
-        script: ".output/server/index.mjs",
-        env: {
-          PORT: 3000, // Nuxt serveri shu portda ishlaydi
-          NODE_ENV: "production"
-        }
+  apps: [
+    {
+      name: "nuxt-app",
+      script: ".output/server/index.mjs",
+      env: {
+        PORT: 3000, // Nuxt serveri shu portda ishlaydi
+        NODE_ENV: "production"
       }
-    ]
-  };
+    }
+  ]
+};
   
