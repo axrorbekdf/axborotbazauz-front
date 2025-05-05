94df2e4eb3cc47c5dd1fb5f1ee47e63c9625fd9e
Ahrorbek Davronov <axrorbektatu@gmail.com>
2025-05-05 17:44:51 +0500

    new updates

--- pages/account/index.vue
+++ pages/account/index.vue
@@ -1,4 +1,26 @@
 <script setup lang="ts">
+useHead({
+  title: 'Xush kelibsiz! - Qidirganizni topasiz!',
+  meta: [
+    // Umumiy meta
+    { name: 'description', content: 'Dars ishlanmalari, diplom ishlari, slaydlar, referatlar, kurs ishlari va fanlar bo‘yicha turli materiallarni yuklab olish yoki yuklash mumkin bo‘lgan bepul ta’lim platformasi — Axborot Baza.' },
+    { name: 'keywords', content: 'dars ishlanmalari, diplom ishlar, kurs ishlari, referatlar, slaydlar, fan materiallari, o‘quv materiallari, bepul yuklab olish, Axborot Baza, ta’lim resurslari, metodik ishlar, maktab darslari, universitet ishlari, pedagogika, ilmiy ishlar, referatlar to‘plami' },
+    { name: 'author', content: 'https://drivesoft.uz' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+
+    // Open Graph (Facebook, Telegram va boshqalar uchun)
+    { property: 'og:title', content: 'axborotbaza.uz - qidirganizni topasiz!' },
+    { property: 'og:description', content: 'Dars ishlanmalari, diplom ishlari, slaydlar, referatlar, kurs ishlari va fanlar bo‘yicha turli materiallarni yuklab olish yoki yuklash mumkin bo‘lgan bepul ta’lim platformasi — Axborot Baza.' },
+    { property: 'og:type', content: 'website' },
+    { property: 'og:url', content: 'https://axborotbaza.uz/' },
+    { property: 'og:image', content: 'https://axborotbaza.uz/favicon.ico' },  // 1200x630 tavsiya etiladi
+
+  ],
+  link: [
+    { rel: 'icon', type: 'image/png', href: '/favicon.ico' }
+  ]
+})
+
 const items = [{
   label: 'Tab1',
   icon: 'i-heroicons-information-circle',
--- pages/index.vue
+++ pages/index.vue
@@ -5,24 +5,18 @@ useHead({
   title: 'Xush kelibsiz! - Qidirganizni topasiz!',
   meta: [
     // Umumiy meta
-    { name: 'description', content: 'Bu sayt Nuxt 3 bilan yaratilgan va SEO uchun optimallashtirilgan.' },
-    { name: 'keywords', content: 'nuxt3, seo, vue, web development' },
-    { name: 'author', content: 'Your Name' },
+    { name: 'description', content: 'Dars ishlanmalari, diplom ishlari, slaydlar, referatlar, kurs ishlari va fanlar bo‘yicha turli materiallarni yuklab olish yoki yuklash mumkin bo‘lgan bepul ta’lim platformasi — Axborot Baza.' },
+    { name: 'keywords', content: 'dars ishlanmalari, diplom ishlar, kurs ishlari, referatlar, slaydlar, fan materiallari, o‘quv materiallari, bepul yuklab olish, Axborot Baza, ta’lim resurslari, metodik ishlar, maktab darslari, universitet ishlari, pedagogika, ilmiy ishlar, referatlar to‘plami' },
+    { name: 'author', content: 'https://drivesoft.uz' },
     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
 
     // Open Graph (Facebook, Telegram va boshqalar uchun)
     { property: 'og:title', content: 'axborotbaza.uz - qidirganizni topasiz!' },
-    { property: 'og:description', content: 'Sayt haqida qisqacha tavsif. Eng yaxshi Nuxt 3 loyihasi.' },
+    { property: 'og:description', content: 'Dars ishlanmalari, diplom ishlari, slaydlar, referatlar, kurs ishlari va fanlar bo‘yicha turli materiallarni yuklab olish yoki yuklash mumkin bo‘lgan bepul ta’lim platformasi — Axborot Baza.' },
     { property: 'og:type', content: 'website' },
     { property: 'og:url', content: 'https://axborotbaza.uz/' },
     { property: 'og:image', content: 'https://axborotbaza.uz/favicon.ico' },  // 1200x630 tavsiya etiladi
 
-    // Twitter Card
-    { name: 'twitter:card', content: 'summary_large_image' },
-    { name: 'twitter:title', content: 'axborotbaza.uz - qidirganizni topasiz!' },
-    { name: 'twitter:description', content: 'Nuxt 3 asosida qurilgan kuchli veb-loyiha.' },
-    { name: 'twitter:image', content: 'https://axborotbaza.uz/favicon.ico' },
-    { name: 'twitter:site', content: '@your_twitter_username' }  // ixtiyoriy
   ],
   link: [
     { rel: 'icon', type: 'image/png', href: '/favicon.ico' }
